93ffe66b21107622323af30068835a321e68ae26
Arshelle9912 <71581324+Arshelle9912@users.noreply.github.com>
2024-07-08 21:01:35 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:abcd1234@localhost:5432/fluttermusicapp')
+DATABASE_URL = "postgresql://data_qqmk_user:mToW3AQnlj1cCoUWeHvmKbGyl21qoXBU@dpg-cq607hl6l47c738scgag-a.oregon-postgres.render.com/data_qqmk"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
