f5d747e96df01a5daa5a6d020e6850360ac664e8
Arshelle9912 <71581324+Arshelle9912@users.noreply.github.com>
2024-07-09 17:25:32 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,14 @@
-import asyncpg
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql+asyncpg://data_qqmk_user:mToW3AQnlj1cCoUWeHvmKbGyl21qoXBU@dpg-cq607hl6l47c738scgag-a.oregon-postgres.render.com/data_qqmk"
+DATABASE_URL = "postgresql://data_qqmk_user:mToW3AQnlj1cCoUWeHvmKbGyl21qoXBU@dpg-cq607hl6l47c738scgag-a.oregon-postgres.render.com/data_qqmk"
 
-async def get_async_db():
-    # Connect to PostgreSQL database asynchronously
-    connection = await asyncpg.connect(DATABASE_URL)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
     try:
-        yield connection
+        yield db
     finally:
-        await connection.close()
+        db.close()
